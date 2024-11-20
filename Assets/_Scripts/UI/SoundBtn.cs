81837ada175603c8d0f163f15df4383d886ff9f4
Stas <stdanshin@gmail.com>
2024-11-20 16:51:52 +0200

    Updated music toggle

--- Assets/_Scripts/UI/SoundBtn.cs
+++ Assets/_Scripts/UI/SoundBtn.cs
@@ -5,6 +5,12 @@ using UnityEngine.UI;
 public class SoundBtn : MonoBehaviour
 {
     [SerializeField]
+    private AudioMixer _audioMixer;
+
+    [SerializeField]
+    private string _musicGroupName = "Music";
+
+    [SerializeField]
     private AudioMixerSnapshot _defaultAudioMixedSnapshot, _mutedAudioMixerSnapshot;
 
     [SerializeField]
@@ -13,7 +19,15 @@ public class SoundBtn : MonoBehaviour
     [SerializeField]
     private Image _buttonIconImage;
 
+
     private bool _soundToggle = true;
+
+    private bool _isMusicOn;
+
+    private void Awake()
+    {
+        UpdateMusic();
+    }
 
     public void ToggleMusic()
     {
@@ -23,5 +37,37 @@ public class SoundBtn : MonoBehaviour
             _defaultAudioMixedSnapshot.TransitionTo(0.1f);
         else
             _mutedAudioMixerSnapshot.TransitionTo(0.1f);
+    }
+
+    public void SetMusicVolume()
+    {
+        _isMusicOn = !_isMusicOn;
+        _buttonIconImage.sprite = _isMusicOn ? _soundOffIcon : _soundOnIcon;
+        if (_audioMixer != null)
+        {
+            float musicVolume;
+            if (_isMusicOn)
+            {
+                musicVolume = -13f;
+                PlayerPrefs.SetInt("music", 0);
+            }
+            else
+            {
+                musicVolume = -80f;
+                PlayerPrefs.SetInt("music", 1);
+            }
+            _audioMixer.SetFloat(_musicGroupName, musicVolume);
+        }
+
+    }
+
+    private void UpdateMusic()
+    {
+        if (PlayerPrefs.HasKey("music"))
+        {
+            _isMusicOn = PlayerPrefs.GetInt("music") == 1 ? true : false; ;
+        }
+        _buttonIconImage.sprite = _isMusicOn ? _soundOffIcon : _soundOnIcon;
+        SetMusicVolume();
     }
 }
